5c8700683b1a9e182f5329939149ad1c7cafd081
RefactorCore <38076986+RefactorCore@users.noreply.github.com>
2025-11-10 20:10:11 +0800

    Add files via upload
    Checkedlist done.

--- models.py
+++ models.py
@@ -29,6 +29,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sku = db.Column(db.String(64), unique=True, nullable=False)
     name = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(50), nullable=True)
     sale_price = db.Column(db.Float, nullable=False, default=0.0)
     cost_price = db.Column(db.Float, nullable=False, default=0.0)
     quantity = db.Column(db.Integer, nullable=False, default=0)
